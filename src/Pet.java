2b4eb9d3775a6775c9d8b3cc186e2dc3a954cf82
Dmitri Mandaji <badmopss@gmail.com>
2023-03-13 16:55:20 +0100

    add the fromString and toString

--- src/Pet.java
+++ src/Pet.java
@@ -49,4 +49,36 @@ public class Pet {
   public void setWeight(double weight) {
     this.weight = weight;
   }
+
+  public static Pet fromString(String input) {
+    String[] parts = input.split(",");
+    Kind kind = Kind.valueOf(parts[0].toUpperCase());
+    String name = parts[1];
+    double weight = 0;
+    LocalDate birthDate = null;
+    if (kind == Kind.CAT) {
+      weight = Double.parseDouble(parts[2]);
+    } else if (kind == Kind.OTHER) {
+      weight = Double.parseDouble(parts[2]);
+      birthDate = LocalDate.parse(parts[3]);
+    } else {
+      // DOG
+      birthDate = LocalDate.parse(parts[2]);
+    }
+    return new Pet(kind, name, birthDate, weight);
+  }
+
+  @Override
+  public String toString() {
+    String str = "Pet: kind=" + kind + ", name='" + name + "'";
+    if (kind == Kind.CAT) {
+      str += ", weight=" + weight;
+    } else if (kind == Kind.OTHER) {
+      str += ", weight=" + weight + ", birthDate=" + birthDate;
+    } else {
+      // DOG
+      str += ", birthDate=" + birthDate;
+    }
+    return str;
+  }
 }
